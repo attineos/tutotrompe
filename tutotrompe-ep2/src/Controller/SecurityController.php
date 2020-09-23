fb0465963abfed6cd69178098a92d044aed863ae
Angot <bat76640@gmail.com>
2020-09-23 23:00:23 +0200

    Transform Response to JSONResponse, like the initial annotation should return

--- tutotrompe-ep2/src/Controller/SecurityController.php
+++ tutotrompe-ep2/src/Controller/SecurityController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class SecurityController extends AbstractController
 {
@@ -18,10 +19,11 @@ class SecurityController extends AbstractController
     {
         $user = $this->getUser();
 
-        return new Response([
+        return new JsonResponse([
             'email' => $user->getEmail(),
-            'roles' => $user->getRoles(),
+            'roles' => $user->getRoles()
         ]);
+
     }
 
     /**
