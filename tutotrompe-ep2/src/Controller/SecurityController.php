69892fd346d50e51749dd7fab6bb51d6125f115f
Julien Verhaeghe <j.verhaeghe@attineos.com>
2020-09-24 15:00:10 +0200

    - Fix function return for api/login_check - Fix composer.json with annotations

--- tutotrompe-ep2/src/Controller/SecurityController.php
+++ tutotrompe-ep2/src/Controller/SecurityController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -18,7 +19,7 @@ class SecurityController extends AbstractController
     {
         $user = $this->getUser();
 
-        return new Response([
+        return new JsonResponse([
             'email' => $user->getEmail(),
             'roles' => $user->getRoles(),
         ]);
